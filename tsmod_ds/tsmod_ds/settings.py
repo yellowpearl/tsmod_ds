c100591bacec55cc2b9b888f537105942a1260f6
yellowpearl <yellowpearl@yandex.ru>
2020-10-23 14:17:37 +0300

    2

--- tsmod_ds/tsmod_ds/settings.py
+++ tsmod_ds/tsmod_ds/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'eg2vea@9&d81lmqciucfy%s2vo1m_4!op49r!=#seew#dc2dlg'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
